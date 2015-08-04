593424bc4cea8d3c6ba3961ecfcdfef2f46cc145
susan06 <susangmedina@gmail.com>
2015-08-04 19:18:53 -0430

    archivos inciales

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-08-04 22:31:15 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-08-04T22:31:15+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
